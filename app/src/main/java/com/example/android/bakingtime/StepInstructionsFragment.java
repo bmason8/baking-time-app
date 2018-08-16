b12a27720d4b3c4af06ed6de6ea99229f1edf219
TERRA\Ben.Mason <ben.mason@terraremote.com>
2018-08-16 10:18:49 -0700

    Video now goes fullscreen when not in tablet/splitscreen mode but gets rotated into landscape mode. Video playback position is now saved and restored through configuration changes.

--- app/src/main/java/com/example/android/bakingtime/StepInstructionsFragment.java
+++ app/src/main/java/com/example/android/bakingtime/StepInstructionsFragment.java
@@ -1,19 +1,23 @@
 package com.example.android.bakingtime;
 
+import android.content.res.Configuration;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.bakingtime.model.Steps;
 import com.example.android.bakingtime.utilities.NetworkUtils;
@@ -30,6 +34,7 @@ import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 
 import java.io.Serializable;
+import java.net.URI;
 import java.util.List;
 
 import butterknife.BindView;
@@ -68,11 +73,8 @@ public class StepInstructionsFragment extends Fragment {
     private Steps mInstructions;
     private List<Steps> mRecipeSteps;
     int position;
+    long videoPlayBackPosition;
     boolean mTwoPane, mSplitView;
-
-//    public StepInstructionsFragment() {
-//    }
-
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -82,6 +84,8 @@ public class StepInstructionsFragment extends Fragment {
             mRecipeSteps = (List<Steps>) savedInstanceState.getSerializable("recipeSteps");
             mInstructions = savedInstanceState.getParcelable("stepInstructions");
             position = savedInstanceState.getInt("position");
+            mSplitView = savedInstanceState.getBoolean("splitView");
+            videoPlayBackPosition = savedInstanceState.getLong("playbackPosition");
         } else {
             Bundle bundle = getArguments();
             if (bundle != null) {
@@ -107,53 +111,37 @@ public class StepInstructionsFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_step_instructions_layout, container, false);
         ButterKnife.bind(this, rootView);
 
-        if (mSplitView) {
-            mButtonCardView.setVisibility(View.GONE);
-        } else {
-            mButtonCardView.setVisibility(View.VISIBLE);
-        }
-
-//        Bundle bundle = getArguments();
-//        if (bundle != null) {
-//            mTwoPane = bundle.getBoolean("twoPane", false);
-//            mRecipeSteps = bundle.getParcelableArrayList("recipeSteps");
-//
-//            if (!mTwoPane) {
-//                // LANDSCAPE/TABLET MODE
-//                position = bundle.getInt("position");
-//            } else {
-//                // PORTRAIT MODE
-//                position = bundle.getInt("position");
-//            }
-//            mInstructions = mRecipeSteps.get(position);
-//        }
-
-
-
-
-//        mPlayerView.setDefaultArtwork(BitmapFactory.decodeResource(
-//                Picasso.with(getContext())
-//                        .load(mInstructions.getThumbnailURL())
-//                        .placeholder(R.drawable.ic_banana)
-//                        .error(R.drawable.ic_banana)
-//                        .into(mPlayerView);
-
-
+        determineLayoutParameters(mSplitView);
 
         updateData();
         return rootView;
     }
 
     @Override
-    public void onStop() {
-        super.onStop();
+    public void onPause() {
+        super.onPause();
+        if (mExoPlayer != null) {
+            videoPlayBackPosition = mExoPlayer.getCurrentPosition();
+        }
         releasePlayer();
     }
 
-    private void initializePlayer(Uri mediaUri) {
+//    @Override
+//    public void onStop() {
+//        super.onStop();
+//        releasePlayer();
+//    }
 
-        Log.d("video", "initializePlayer ran");
-        // need to set up an if/else statement for replacing the default exoPlayer view with a thumbnail and no controls if there is no mediaUri
+//    @Override
+//    public void onResume() {
+//        super.onResume();
+//        Uri videoUri = NetworkUtils.convertStringToUri(mInstructions.getVideoURL());
+//        if (videoUri != null) {
+//            initializePlayer(videoUri);
+//        }
+//    }
+
+    private void initializePlayer(Uri mediaUri) {
 
         if (mExoPlayer == null) {
             Log.d("video", "mExoplayer is null");
@@ -162,19 +150,25 @@ public class StepInstructionsFragment extends Fragment {
             LoadControl loadControl = new DefaultLoadControl();
             mExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector, loadControl);
             mPlayerView.setUseController(true);
+            if (videoPlayBackPosition > 0) {
+                mExoPlayer.seekTo(videoPlayBackPosition);
+            }
             mPlayerView.setPlayer(mExoPlayer);
             // Prepare the MediaSource
             String userAgent = "temp";
             MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(
                     getContext(), userAgent), new DefaultExtractorsFactory(), null, null);
             mExoPlayer.prepare(mediaSource);
+
             // true plays video right away, false requires pressing play
             mExoPlayer.setPlayWhenReady(false);
+
         }
     }
 
     private void releasePlayer() {
         if (mExoPlayer != null) {
+//            videoPlayBackPosition = mExoPlayer.getCurrentPosition();
             mExoPlayer.stop();
             mExoPlayer.release();
             mExoPlayer = null;
@@ -197,7 +191,6 @@ public class StepInstructionsFragment extends Fragment {
             mPlayerView.setUseController(false);
             mPlayerView.setDefaultArtwork(BitmapFactory.decodeResource(getResources(), R.drawable.no_video));
             mPlayerView.setPlayer(mExoPlayer);
-
         }
     }
 
@@ -206,6 +199,7 @@ public class StepInstructionsFragment extends Fragment {
             position -= 1;
         }
         mInstructions = mRecipeSteps.get(position);
+        videoPlayBackPosition = 0;
         releasePlayer();
         updateData();
     }
@@ -215,15 +209,10 @@ public class StepInstructionsFragment extends Fragment {
             position += 1;
         }
         mInstructions = mRecipeSteps.get(position);
+        videoPlayBackPosition = 0;
         releasePlayer();
         updateData();
     }
-
-//    protected void positionUpdate(int newPosition) {
-//        Toast.makeText(getContext(), "positionUpdate Ran!", Toast.LENGTH_SHORT).show();
-//        position = newPosition;
-//        updateData();
-//    }
 
     public void positionUpdateFromInterface(int newPosition) {
         position = newPosition;
@@ -238,6 +227,25 @@ public class StepInstructionsFragment extends Fragment {
         outState.putSerializable("recipeSteps", (Serializable) mRecipeSteps);
         outState.putParcelable("stepInstructions", mInstructions);
         outState.putInt("position", position);
+        outState.putBoolean("splitView", mSplitView);
+        outState.putLong("playbackPosition", videoPlayBackPosition);
+    }
+
+// https://stackoverflow.com/questions/2591036/how-to-hide-the-title-bar-for-an-activity-in-xml-with-existing-custom-theme
+    private void determineLayoutParameters(boolean splitScreen) {
+        if (splitScreen) {
+            mButtonCardView.setVisibility(View.GONE);
+        } else {
+            if (getActivity().getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                mButtonCardView.setVisibility(View.GONE);
+                getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                if (((AppCompatActivity)getActivity()).getSupportActionBar() != null) {
+                    ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+                }
+            } else {
+                mButtonCardView.setVisibility(View.VISIBLE);
+            }
+        }
     }
 
     //    @Override
